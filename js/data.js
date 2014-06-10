badb94e39cdb5b43c1be211de82bb5de45197d50
Bij4n <b@mawji.vc>
2014-06-10 19:55:00 -0400

    add name data arrays - first names and last names

---
+++ js/data.js
@@ -0,0 +1,33 @@
+var MALE_FIRST_NAMES = [
+    'James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard',
+    'Joseph', 'Thomas', 'Charles', 'Christopher', 'Daniel', 'Matthew',
+    'Anthony', 'Mark', 'Donald', 'Steven', 'Paul', 'Andrew', 'Joshua',
+    'Kenneth', 'Kevin', 'Brian', 'George', 'Timothy', 'Ronald', 'Edward',
+    'Jason', 'Jeffrey', 'Ryan', 'Jacob', 'Gary', 'Nicholas', 'Eric',
+    'Jonathan', 'Stephen', 'Larry', 'Justin', 'Scott', 'Brandon'
+];
+
+var FEMALE_FIRST_NAMES = [
+    'Mary', 'Patricia', 'Jennifer', 'Linda', 'Barbara', 'Elizabeth',
+    'Susan', 'Jessica', 'Sarah', 'Karen', 'Lisa', 'Nancy', 'Betty',
+    'Margaret', 'Sandra', 'Ashley', 'Dorothy', 'Kimberly', 'Emily',
+    'Donna', 'Michelle', 'Carol', 'Amanda', 'Melissa', 'Deborah',
+    'Stephanie', 'Rebecca', 'Sharon', 'Laura', 'Cynthia', 'Kathleen',
+    'Amy', 'Angela', 'Shirley', 'Anna', 'Brenda', 'Pamela', 'Emma',
+    'Nicole', 'Helen'
+];
+
+var LAST_NAMES = [
+    'Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller',
+    'Davis', 'Rodriguez', 'Martinez', 'Hernandez', 'Lopez', 'Gonzalez',
+    'Wilson', 'Anderson', 'Thomas', 'Taylor', 'Moore', 'Jackson', 'Martin',
+    'Lee', 'Perez', 'Thompson', 'White', 'Harris', 'Sanchez', 'Clark',
+    'Ramirez', 'Lewis', 'Robinson', 'Walker', 'Young', 'Allen', 'King',
+    'Wright', 'Scott', 'Torres', 'Nguyen', 'Hill', 'Flores', 'Green',
+    'Adams', 'Nelson', 'Baker', 'Hall', 'Rivera', 'Campbell', 'Mitchell',
+    'Carter', 'Roberts', 'Gomez', 'Phillips', 'Evans', 'Turner', 'Diaz',
+    'Parker', 'Cruz', 'Edwards', 'Collins', 'Reyes', 'Stewart', 'Morris',
+    'Morales', 'Murphy', 'Cook', 'Rogers', 'Gutierrez', 'Ortiz', 'Morgan',
+    'Cooper', 'Peterson', 'Bailey', 'Reed', 'Kelly', 'Howard', 'Ramos',
+    'Kim', 'Cox', 'Ward'
+];
