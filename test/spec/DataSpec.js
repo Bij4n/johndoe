195ab621e7a26a5339bd6d732b13162a90a17b75
Bij4n <b@mawji.vc>
2014-06-10 20:40:00 -0400

    add failing specs for location data

--- test/spec/DataSpec.js
+++ test/spec/DataSpec.js
@@ -33,3 +33,34 @@ describe('Name Data', function() {
         });
     });
 });
+
+describe('Location Data', function() {
+
+    it('should have all 50 states', function() {
+        expect(STATES).toBeDefined();
+        expect(STATES.length).toBe(50);
+    });
+
+    it('should have state objects with name and abbreviation', function() {
+        STATES.forEach(function(state) {
+            expect(state.name).toBeDefined();
+            expect(state.abbr).toBeDefined();
+            expect(state.abbr.length).toBe(2);
+        });
+    });
+
+    it('should have cities for every state', function() {
+        expect(CITIES_BY_STATE).toBeDefined();
+        STATES.forEach(function(state) {
+            var stateData = CITIES_BY_STATE[state.abbr];
+            expect(stateData).toBeDefined();
+            expect(stateData.cities.length).toBeGreaterThan(0);
+            expect(stateData.zipStart).toBeDefined();
+        });
+    });
+
+    it('should have street names', function() {
+        expect(STREET_NAMES).toBeDefined();
+        expect(STREET_NAMES.length).toBeGreaterThan(20);
+    });
+});
